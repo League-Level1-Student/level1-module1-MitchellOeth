25ad2acb5ddb0b684e5cb44a436c0a945f7d7988
League <League@adminisorsimac2.attlocal.net>
2018-06-16 10:40:23 -0700

    smurf

---
+++ src/SmurfRunner.java
@@ -0,0 +1,23 @@
+
+public class SmurfRunner {
+	public static void main(String[] args) {
+		
+	
+Smurf handy = new Smurf("handy");
+handy.getName();
+handy.eat();
+
+
+Smurf Papa = new Smurf("Papa");
+Papa.getName();
+Papa.getHatColor();
+Papa.isGirlOrBoy();
+
+Smurf smurfette = new Smurf("smurfette");
+smurfette.getName();
+smurfette.getHatColor();
+smurfette.isGirlOrBoy();
+
+
+}
+}
